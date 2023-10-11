a599ad6d7d495dc346cb93e6746955b1bc893620
Eric <f102041332@gmail.com>
2023-10-11 17:33:41 +0800

    [add] test api

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,15 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get("/", function (req, res, next) {
+	res.status(200).json({
+		error_code: 0,
+		data: {
+			x: 1,
+			y: 2,
+		},
+	});
 });
 
 module.exports = router;
